06ce72271690494bcf07507103f47f8e5615e150
Steven Simon <Steven.Simon@noaa.gov>
2023-07-03 19:11:01 +0000

    Add trigger scripts to call global_ens wave driver scripts at each step.

---
+++ trigger_scripts/global_ens_wave_plots_trigger_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /lfs/h2/emc/ptmp/steve.simon/evs_out
+qsub /lfs/h2/emc/vpppg/noscrub/steven.simon/gitworkspace/EVS/dev/drivers/scripts/global_ens/prep/jevs_global_ens_wave_grid2obs_plots.sh
