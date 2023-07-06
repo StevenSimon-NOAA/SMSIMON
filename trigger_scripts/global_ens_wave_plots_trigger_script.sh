7a7a902b56720654e85c39418c119ddfdcadf61e
Steven Simon <Steven.Simon@noaa.gov>
2023-07-06 19:14:38 +0000

    Modify global_ens waves stats and plots trigger scripts qsub paths.

--- trigger_scripts/global_ens_wave_plots_trigger_script.sh
+++ trigger_scripts/global_ens_wave_plots_trigger_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /lfs/h2/emc/ptmp/steven.simon/evs_out
-qsub /lfs/h2/emc/vpppg/noscrub/steven.simon/gitworkspace/EVS/dev/drivers/scripts/global_ens/prep/jevs_global_ens_wave_grid2obs_plots.sh
+qsub /lfs/h2/emc/vpppg/noscrub/steven.simon/gitworkspace/EVS/dev/drivers/scripts/global_ens/plots/jevs_global_ens_wave_grid2obs_plots.sh
