61410af7ce2b26a97c40a8e6ca8889cca18998c4
Steven Simon <Steven.Simon@noaa.gov>
2023-07-21 20:12:18 +0000

    Renamed global_ens_gefs_atmos_prep_trigger_script.sh -> global_ens_atmos_prep_trigger_script.sh.

--- trigger_scripts/global_ens_gefs_atmos_prep_trigger_script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /lfs/h2/emc/ptmp/steven.simon/evs_out
-qsub /lfs/h2/emc/vpppg/noscrub/steven.simon/gitworkspace/EVS/dev/drivers/scripts/global_ens/prep/jevs_global_ens_gefs_atmos_prep.sh 
