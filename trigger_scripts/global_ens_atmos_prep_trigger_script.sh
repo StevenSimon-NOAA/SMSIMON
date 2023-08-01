7b6c9846069c6d274267e2442dc206d598b14f41
Steven Simon <Steven.Simon@noaa.gov>
2023-08-01 21:13:06 +0000

    Update qsub jobs by elimnating gitworkspace from their paths.

--- trigger_scripts/global_ens_atmos_prep_trigger_script.sh
+++ trigger_scripts/global_ens_atmos_prep_trigger_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /lfs/h2/emc/ptmp/steven.simon/evs_out
-qsub /lfs/h2/emc/vpppg/noscrub/steven.simon/gitworkspace/EVS/dev/drivers/scripts/global_ens/prep/jevs_global_ens_atmos_prep.sh 
+qsub /lfs/h2/emc/vpppg/noscrub/steven.simon/EVS/dev/drivers/scripts/global_ens/prep/jevs_global_ens_atmos_prep.sh 
